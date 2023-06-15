4cb2fbee9d3ca5a199ba8914fefc49dc3179185b
becem gharbi <becem.gharbi@live.com>
2023-06-15 14:10:44 +0100

    feat: Implement report settings update

--- components/Settings/Report.vue
+++ components/Settings/Report.vue
@@ -4,20 +4,8 @@
             <n-input v-model:value="model.webhookUrl"></n-input>
         </n-form-item>
 
-        <n-form-item label="Bugsnag URL" path="bugsnagUrl">
-            <n-input v-model:value="model"></n-input>
-        </n-form-item>
-
-        <n-form-item label="Username" path="username">
-            <n-input v-model:value="model.username"></n-input>
-        </n-form-item>
-
-        <n-form-item label="Password" path="password">
-            <n-input v-model:value="model.password"></n-input>
-        </n-form-item>
-
-        <n-form-item label="Certificate" path="certificate">
-            <n-input type="textarea" autosize v-model:value="model.certificate"></n-input>
+        <n-form-item label="Bugsnag Key" path="bugsnagUrl">
+            <n-input v-model:value="model.bugsnagKey"></n-input>
         </n-form-item>
 
         <div class="flex gap-4">
@@ -29,50 +17,22 @@
 
 <script setup lang="ts">
 
-const { apiErrors, formRef, onSubmit, pending, rules } = useNaiveForm()
+const { formRef, onSubmit, pending, rules } = useNaiveForm()
 
 const { find } = useReport()
 
 const { data: report } = await find()
 
 const model = ref<Partial<Report>>({
-    bagsnagKey: report.value?.bagsnagKey,
+    bugsnagKey: report.value?.bugsnagKey,
     webhookUrl: report.value?.webhookUrl
 });
 
-rules.value = {
-    uriWS: [
-        {
-            required: true,
-            message: "Please enter borker's Websockets URI",
-            trigger: "blur",
-        },
-        {
-            message: "Unable to connect",
-            validator: () => !apiErrors.value.unableToConnect
-        },
-    ],
-    username: [
-        {
-            required: true,
-            message: "Please enter username",
-            trigger: "blur",
-        },
-    ],
-    password: [
-        {
-            required: true,
-            message: "Please enter password",
-            trigger: "blur",
-        },
-    ],
-}
-
 
 async function handleSubmit() {
-    const { add, update } = useMqtt()
+    const { add, update } = useReport()
 
-    if (mqtt.value) {
+    if (report.value) {
         const { data, error } = await update(model.value)
 
     }

b93760d2a740a887bbcd222983adc071e512b001
becem gharbi <becem.gharbi@live.com>
2023-06-15 14:12:00 +0100

    chore: Enable ssr only in production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { auth, naiveui, tailwindcss, s3 } from "./config";
 
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: process.env.NODE_ENV === "production",
 
   app: {
     head: {

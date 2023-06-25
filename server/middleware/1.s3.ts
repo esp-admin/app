7a16fea133484ff036ccef26c8245d52c2bbafde
becem gharbi <becem.gharbi@live.com>
2023-06-25 19:32:47 +0100

    fix: Fix S3 middleware

--- server/middleware/1.s3.ts
+++ server/middleware/1.s3.ts
@@ -1,20 +1,22 @@
 import { setPermissions } from "#s3";
 
-export default defineEventHandler((event) => {
-  const role = event.context.auth?.userRole;
+export default defineEventHandler(async (event) => {
+  try {
+    const { userRole } = checkUser(event);
 
-  setPermissions(event, {
-    bucket: {
-      create: false,
-      delete: false,
-      list: false,
-    },
-    object: {
-      create: role === "admin" || role === "user",
-      delete: role === "admin" || role === "user",
-      list: false,
-      read: role === "admin" || role === "user",
-      update: role === "admin" || role === "user",
-    },
-  });
+    setPermissions(event, {
+      bucket: {
+        create: false,
+        delete: false,
+        list: false,
+      },
+      object: {
+        create: userRole === "admin" || userRole === "user",
+        delete: userRole === "admin" || userRole === "user",
+        list: false,
+        read: userRole === "admin" || userRole === "user",
+        update: userRole === "admin" || userRole === "user",
+      },
+    });
+  } catch (error) {}
 });

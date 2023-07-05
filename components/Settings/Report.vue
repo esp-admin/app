91b3f050adc34679462dc44a76430b79f24594bc
becem gharbi <becem.gharbi@live.com>
2023-07-05 16:10:36 +0100

    refactor: Update report settings form

--- components/Settings/Report.vue
+++ components/Settings/Report.vue
@@ -1,11 +1,20 @@
 <template>
     <n-form ref="formRef" :rules="rules" :model="model" @submit.prevent="() => onSubmit(handleSubmit)">
+        <n-form-item label="Report by webhook" path="webhookEnable" label-placement="left">
+            <n-checkbox v-model:checked="model.webhookEnable"></n-checkbox>
+        </n-form-item>
+
         <n-form-item label="Webhook URL" path="webhookUrl">
             <n-input v-model:value="model.webhookUrl"></n-input>
         </n-form-item>
 
-        <n-form-item label="Bugsnag Key" path="bugsnagUrl">
-            <n-input v-model:value="model.bugsnagKey"></n-input>
+
+        <n-form-item label="Report by email" path="emailEnable" label-placement="left">
+            <n-checkbox v-model:checked="model.emailEnable"></n-checkbox>
+        </n-form-item>
+
+        <n-form-item label="Email" path="emailAddress">
+            <n-input v-model:value="model.emailAddress"></n-input>
         </n-form-item>
 
         <FormButtons @reset="reset" :loading="pending" :disabled="!edited || pending"></FormButtons>
@@ -20,11 +29,32 @@ const { find } = useReport()
 const report = await find()
 
 const model = ref<Partial<Report>>({
-    bugsnagKey: report.value?.bugsnagKey,
-    webhookUrl: report.value?.webhookUrl
+    webhookEnable: report.value.webhookEnable,
+    webhookUrl: report.value.webhookUrl,
+    emailEnable: report.value.emailEnable,
+    emailAddress: report.value.emailAddress
 });
 
 const { formRef, onSubmit, pending, rules, edited, reset } = useNaiveForm(model)
+
+rules.value = {
+    webhookUrl: [
+        {
+            message: "Webhook URL required if enabled",
+            validator: (rule, value) => model.value.webhookEnable ? !!value : true
+        },
+    ],
+    emailAddress: [
+        {
+            type: "email",
+            message: "Should be a valid email address"
+        },
+        {
+            message: "Email address required if enabled",
+            validator: (rule, value) => model.value.emailEnable ? !!value : true
+        },
+    ],
+}
 
 async function handleSubmit() {
     const { add, update } = useReport()

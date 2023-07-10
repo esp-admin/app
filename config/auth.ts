04da2d3dc2d2bdffd7eba5c2557ae3a2c08a3be6
becem gharbi <becem.gharbi@live.com>
2023-07-10 12:30:42 +0100

    refactor: Customize email templates

--- config/auth.ts
+++ config/auth.ts
@@ -57,4 +57,108 @@ export const auth: Partial<ModuleOptions> = {
     pass: process.env.AUTH_SMTP_PASS || "",
     from: process.env.AUTH_SMTP_FROM || "",
   },
+
+  emailTemplates: {
+    emailVerify: `
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #f1f5f9;
+            color: #0f172a;
+            font-family: "Arial";
+            padding: 8px;
+        }
+
+        a {
+            cursor: pointer;
+            display: block;
+            text-align: center;
+            color: #4438cac5;
+            margin: 8px;
+        }
+
+        header {
+            text-align: center;
+            padding: 8px;
+            background-color: rgba(0, 0, 255, 0.295);
+        }
+    </style>
+  </head>
+
+  <body>
+    <header>
+        <h3>ESP Admin</h3>
+    </header>
+
+    <h3>Hello {{name}},</h3>
+    <p>
+        We have received a request to verify your email address. If you haven't
+        made this request please ignore the following email.
+    </p>
+    <p>
+        Otherwise, to complete the process, click the following link.
+    </p>
+    <a href="{{link}}">Verify your email</a>
+    <b>Important, this link will expire in {{validityInMinutes}} minutes.</b>
+    <p>Thank you, and have a good day.</p>
+  </body>
+
+  </html>
+    `,
+    passwordReset: `
+  <html lang="en">
+
+  <head>
+    <style>
+        body {
+            background-color: #f1f5f9;
+            color: #0f172a;
+            font-family: "Arial";
+            padding: 8px;
+        }
+
+        a {
+            cursor: pointer;
+            display: block;
+            text-align: center;
+            color: #4338ca;
+            margin: 24px;
+        }
+
+
+        header {
+            text-align: center;
+            padding: 8px;
+            background-color: rgba(0, 0, 255, 0.295);
+        }
+    </style>
+  </head>
+
+  <body>
+    <header>
+        <h3>ESP Admin</h3>
+    </header>
+
+    <h3>Hello {{name}},</h3>
+    <p>
+        We have received a request to reset your password. If you haven't
+        made this request please ignore the following email.
+    </p>
+    <p>
+        Otherwise, to complete the process, click the following link.
+    </p>
+    <a href="{{link}}">Reset your password</a>
+    <b>Important, this link will expire in {{validityInMinutes}} minutes.</b>
+    <p>Thank you, and have a good day.</p>
+  </body>
+
+  </html>
+    `,
+  },
 };

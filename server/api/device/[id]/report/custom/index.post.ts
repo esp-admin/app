4cde7e9854e79e58b40605f7b55425ae914c0370
becem gharbi <becem.gharbi@live.com>
2023-07-11 11:49:39 +0100

    feat: Create custom report email template

--- server/api/device/[id]/report/custom/index.post.ts
+++ server/api/device/[id]/report/custom/index.post.ts
@@ -1,10 +1,11 @@
 import { handleError, prisma, sendMail } from "#auth";
+import Mustache from "mustache";
 
 export default defineEventHandler(async (event) => {
   interface Body {
     type: "info" | "error" | "warn" | "success";
-    subject: String;
-    body: String;
+    subject: string;
+    body: string;
   }
 
   try {
@@ -24,9 +25,12 @@ export default defineEventHandler(async (event) => {
 
     if (report.emailEnable && report.emailAddress) {
       sendMail({
-        subject: `[${message.type}] ${message.subject}`,
+        subject: `${message.type} | ${message.subject}`,
         to: report.emailAddress,
-        html: `<p>${message.body}</p>`,
+        html: Mustache.render(reportTemplate, {
+          type: message.type,
+          body: message.body,
+        }),
       });
     }
 
@@ -46,3 +50,60 @@ export default defineEventHandler(async (event) => {
     await handleError(error);
   }
 });
+
+const reportTemplate = `
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <style>
+        body {
+            background-color: #f1f5f9;
+            color: #0f172a;
+            font-family: "Arial";
+            padding: 8px;
+        }
+
+        a {
+            cursor: pointer;
+            display: block;
+            text-align: center;
+            color: #4438cac5;
+            margin: 8px;
+        }
+
+        header {
+            text-align: center;
+            padding: 8px;
+            background-color: rgba(0, 0, 255, 0.295);
+        }
+
+        .type {
+            font-weight: bold;
+            display: inline;
+        }
+
+        .payload {
+            font-style: italic;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h3>ESP Admin</h3>
+    </header>
+
+    <h3>Hello</h3>
+
+    <span>We have received <p class="type">{{type}}</p> custom report message with the following payload</span>
+
+    <p class="payload">
+        {{body}}
+    </p>
+</body>
+
+</html>`;

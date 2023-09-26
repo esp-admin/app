07005248671c9a748643e2670fe11b5dee52c43f
becem-gharbi <becem.gharbi@live.com>
2023-09-26 11:58:15 +0100

    refactor: Update default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="page">
     <template v-if="user">
       <LazyNavbar />
-      <div class="flex-1 container mx-auto my-8 px-4">
+      <div class="flex-1 container mx-auto mt-8 px-4">
         <slot />
       </div>
       <LazyFooter />

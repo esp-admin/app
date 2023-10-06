d4110f09a7dc9866d5b10c06dec8f07636899321
becem-gharbi <becem.gharbi@live.com>
2023-10-06 15:26:26 +0100

    chore(s3): Set upload max size

--- config/s3.ts
+++ config/s3.ts
@@ -1,11 +1,7 @@
 import type { ModuleOptions } from 'nuxt-s3'
 
 export const s3: Partial<ModuleOptions> = {
-  driver: process.env.NUXT_S3_DRIVER as 's3' | 'fs',
-  region: '*',
-  endpoint: '*',
-  accessKeyId: '*',
-  secretAccessKey: '*',
-  bucket: '*',
+  driver: 'fs',
+  maxSizeMb: 5,
   accept: '^(image/(png|jpeg|png|gif))|(application/octet-stream)'
 }

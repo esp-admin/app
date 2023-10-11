b1404a1f06aa83898e84e18fe539848efd632ef0
becem-gharbi <becem.gharbi@live.com>
2023-10-11 12:22:42 +0100

    fix: Run Sync on loggedIn and stop on loggedOut

--- plugins/sync.client.ts
+++ plugins/sync.client.ts
@@ -1,29 +1,38 @@
-export default defineNuxtPlugin((nuxtApp) => {
-  const deploymentTimemoutMs = 3 * 60 * 1000
-  const syncIntervalMs = 10 * 1000
+export default defineNuxtPlugin({
+  enforce: 'pre',
 
-  nuxtApp.hook('app:mounted', () => {
-    setInterval(sync, syncIntervalMs)
-  })
+  setup: (nuxtApp) => {
+    const deploymentTimemoutMs = 3 * 60 * 1000
+    const syncIntervalMs = 10 * 1000
+    let intervalId:any
 
-  function sync () {
-    updateDeployments()
-  }
+    nuxtApp.hook('auth:loggedIn', (loggedIn) => {
+      if (loggedIn) {
+        intervalId = setInterval(sync, syncIntervalMs)
+      } else if (intervalId) {
+        clearInterval(intervalId)
+      }
+    })
 
-  function updateDeployments () {
-    const { devices } = useDevice()
+    function sync () {
+      updateDeployments()
+    }
 
-    for (const device of devices.value || []) {
-      const { deployments, update } = useDeployment(device.id)
+    function updateDeployments () {
+      const { devices } = useDevice()
 
-      for (const deployment of deployments.value || []) {
-        if (deployment.status === 'started') {
-          const deploymentStartTimestamp = new Date(deployment.createdAt).getTime()
-          const maxDeploymentEndTimestamp = deploymentStartTimestamp + deploymentTimemoutMs
-          const now = new Date().getTime()
+      for (const device of devices.value || []) {
+        const { deployments, update } = useDeployment(device.id)
 
-          if (maxDeploymentEndTimestamp < now) {
-            update(deployment.id, 'failed', true)
+        for (const deployment of deployments.value || []) {
+          if (deployment.status === 'started') {
+            const deploymentStartTimestamp = new Date(deployment.createdAt).getTime()
+            const maxDeploymentEndTimestamp = deploymentStartTimestamp + deploymentTimemoutMs
+            const now = new Date().getTime()
+
+            if (maxDeploymentEndTimestamp < now) {
+              update(deployment.id, 'failed', true)
+            }
           }
         }
       }

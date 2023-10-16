25e52520209e5113fd89bb4c845a2feac791bfc6
becem-gharbi <becem.gharbi@live.com>
2023-10-16 08:50:36 +0100

    fix(TitleDate): Update each 10 seconds

--- components/Title/Date.vue
+++ components/Title/Date.vue
@@ -8,16 +8,22 @@
       {{ title }}
     </n-text>
 
-    <n-text v-if="createdAt" depth="3" class="text-base font-normal">
-      Created <n-time :time="new Date(createdAt)" type="relative" />
-    </n-text>
+    <div :key="key">
+      <n-text v-if="createdAt" depth="3" class="text-base font-normal">
+        Created <n-time :time="new Date(createdAt)" type="relative" />
+      </n-text>
 
-    <n-text v-if="updatedAt" depth="3" class="text-base font-normal">
-      Updated <n-time :time="new Date(updatedAt)" type="relative" />
-    </n-text>
+      <n-text v-else-if="updatedAt" depth="3" class="text-base font-normal">
+        Updated <n-time :time="new Date(updatedAt)" type="relative" />
+      </n-text>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps<{title:string, icon?:string, createdAt?: string|Date, updatedAt?:string|Date}>()
+defineProps<{ title: string, icon?: string, createdAt?: string | Date, updatedAt?: string | Date }>()
+
+const key = ref(0)
+
+onMounted(() => setInterval(() => key.value++, 10000))
 </script>

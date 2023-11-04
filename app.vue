f415f4850f4f6b2b5e00dc49fa5971452793f237
becem-gharbi <becem.gharbi@live.com>
2023-11-04 13:38:16 +0100

    fix: Add missing html tags

--- app.vue
+++ app.vue
@@ -187,6 +187,19 @@ useSeoMeta({
   twitterImage: '/favicon.ico',
   twitterCard: 'summary'
 })
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
 </script>
 
 <style>

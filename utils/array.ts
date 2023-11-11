14d6009ebab3dbddb70c41c060dcd53f9df0411c
becem-gharbi <becem.gharbi@live.com>
2023-11-11 16:27:29 +0100

    fix(removeArrayElByKey): Remove all elements passing condition

--- utils/array.ts
+++ utils/array.ts
@@ -2,12 +2,10 @@
  * Removes element from array having a certain value for a giving key
  */
 export function removeArrayElByKey (array: Array<Record<string, any>>, key: string, value: any) {
-  const index = array.findIndex(
-    el => el[key] === value
-  )
-
-  if (index >= 0) {
-    array.splice(index, 1)
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i][key] === value) {
+      array.splice(i, 1)
+    }
   }
 }
 

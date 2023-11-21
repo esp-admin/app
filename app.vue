720b3e83039bc4a7d5f1cb51755afb1ccf7be62f
becem-gharbi <becem.gharbi@live.com>
2023-11-21 17:29:51 +0100

    refactor: Move transition styles to assets

--- app.vue
+++ app.vue
@@ -200,27 +200,3 @@ useSeoMeta({
   twitterCard: 'summary'
 })
 </script>
-
-<style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.2s;
-}
-
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-  filter: input(1rem);
-}
-
-.layout-enter-active,
-.layout-leave-active {
-  transition: all 0.2s;
-}
-
-.layout-enter-from,
-.layout-leave-to {
-  opacity: 0;
-  filter: input(1rem);
-}
-</style>

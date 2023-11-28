2f3c5198a9ea1bb3f3ea0e6f7407536671e587fc
becem-gharbi <becem.gharbi@live.com>
2023-11-28 11:11:47 +0100

    refactor(FormButtons): reverse order

--- components/Form/Buttons.vue
+++ components/Form/Buttons.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex gap-4">
-    <n-button type="primary" attr-type="submit" :loading="loading" :disabled="disabled">
-      Save
-    </n-button>
     <n-button secondary attr-type="reset" :disabled="disabled" @click="$emit('reset')">
       Reset
+    </n-button>
+    <n-button type="primary" attr-type="submit" :loading="loading" :disabled="disabled">
+      Save
     </n-button>
   </div>
 </template>

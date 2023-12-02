1b49aad8550b7a941f7a6c1f03232479f06671f4
becem-gharbi <becem.gharbi@live.com>
2023-12-02 07:34:45 +0100

    refactor: Remove s3 middleware fixing encoding on netlify

--- server/middleware/s3.ts
+++ server/middleware/s3.ts
@@ -6,18 +6,6 @@ export default defineEventHandler((event) => {
 
   if (isS3Mutation) {
     checkUpload(event)
-
-    // https://github.com/unjs/nitro/issues/1719
-    const isBase64 = (value: string) => {
-      if (typeof value !== 'string') { return false }
-      return /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/.test(
-        value
-      )
-    }
-
-    if (isBase64(event.node.req.body)) {
-      event.node.req.body = Buffer.from(event.node.req.body, 'base64')
-    }
   } else if (isS3Query) {
     setResponseHeader(event, 'Cache-Control', 'public, max-age=2592000, immutable')
   }

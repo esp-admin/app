4fe854f8d7edb8258afcc9a752b85737dc259cf5
becem-gharbi <becem.gharbi@live.com>
2023-12-07 15:23:56 +0100

    refactor(default layout): use naive-ui scrollbar over the native one

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="page">
     <template v-if="user">
-      <Navbar />
-
       <Notification />
 
-      <div class="flex-1 container mx-auto mt-6 mb-4 p-4">
-        <slot />
-      </div>
+      <n-layout :native-scrollbar="false" style="height: 100dvh">
+        <Navbar />
+        <div class="flex-1 container mx-auto mt-6 mb-4 p-4">
+          <slot />
+        </div>
+      </n-layout>
     </template>
   </div>
 </template>

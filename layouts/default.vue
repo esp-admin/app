3a9ccfc20cc90ee68d3e27e6ef94afcbfd3404ad
becem-gharbi <becem.gharbi@live.com>
2023-12-07 18:58:37 +0100

    refactor: Hide scrollbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,10 @@
   <div class="page">
     <template v-if="user">
       <Notification />
-
-      <n-layout :native-scrollbar="false" style="height: 100dvh">
-        <Navbar />
-        <div class="flex-1 container mx-auto mt-6 mb-4 p-4">
-          <slot />
-        </div>
-      </n-layout>
+      <Navbar />
+      <div class="flex-1 container mx-auto mt-6 mb-4 p-4">
+        <slot />
+      </div>
     </template>
   </div>
 </template>

9c5d3545838ef1d4ea10432b1ed22d1bf0b1ec6f
becem-gharbi <becem.gharbi@live.com>
2023-12-14 09:09:11 +0100

    refactor: change default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,66 @@
 <template>
-  <div class="page">
-    <template v-if="user">
-      <Notification />
+  <div>
+    <naive-layout-sidebar v-if="user" :routes="routes" :drawer-routes="routes">
+      <template #start>
+        <nuxt-link to="/">
+          <Logo :width="110" class="mx-auto my-2" />
+        </nuxt-link>
+      </template>
 
-      <Navbar />
-      <div class="flex-1 container mx-auto mt-6 mb-4 p-4">
+      <template #end>
+        <n-button secondary class="m-2 notMobileOrTablet" @click="logout">
+          Logout
+        </n-button>
+      </template>
+
+      <template #drawer-header>
+        <AccountInfo class="mx-2" />
+      </template>
+
+      <template #drawer-footer>
+        <n-button secondary block @click="logout">
+          Logout
+        </n-button>
+      </template>
+
+      <div class="p-2">
         <slot />
       </div>
-    </template>
+    </naive-layout-sidebar>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { MenuLinkRoute } from '@bg-dev/nuxt-naiveui'
+
 const { user } = useAuthSession()
+const { logout } = useAuth()
+
+const routes: MenuLinkRoute[] = [
+  {
+    label: 'Dashboard',
+    icon: ICON_DASH,
+    path: '/'
+  },
+  {
+    label: 'Projects',
+    icon: ICON_PROJECT,
+    path: '/projects'
+  },
+  {
+    label: 'Devices',
+    icon: ICON_DEVICE,
+    path: '/devices'
+  },
+  {
+    label: 'Settings',
+    icon: ICON_SETTINGS,
+    path: '/settings'
+  },
+  {
+    label: 'Account',
+    path: '/account',
+    icon: ICON_USER
+  }
+]
 </script>

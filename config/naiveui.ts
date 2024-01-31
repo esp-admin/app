7becfcfe0d43cb3f04b27fd15e1458c219c23881
becem-gharbi <becem.gharbi@live.com>
2024-01-31 14:50:42 +0100

    chore(deps): upgrade nuxt-naiveui to 1.10.1-edge

--- config/naiveui.ts
+++ config/naiveui.ts
@@ -2,5 +2,6 @@ import type { ModuleOptions } from '@bg-dev/nuxt-naiveui'
 
 export const naiveui: Partial<ModuleOptions> = {
   colorModePreference: 'light',
-  iconSize: 18
+  iconSize: 18,
+  iconDownload: true
 }

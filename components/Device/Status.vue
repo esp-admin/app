30733149f33c46a47b8943eef30c44973537daba
becem-gharbi <becem.gharbi@live.com>
2024-05-20 15:24:37 +0100

    revert: reactivity of status components

--- components/Device/Status.vue
+++ components/Device/Status.vue
@@ -1,22 +1,24 @@
 <template>
   <naive-icon
-    :name="icon.name"
+    v-if="device.status === 'connected'"
+    :name="ICON_CHECK"
     :size="size"
-    :icon-color="icon.color"
+    icon-color="#22c55e"
+  />
+  <naive-icon
+    v-if="device.status === 'disconnected'"
+    :name="ICON_FAIL"
+    :size="size"
+    icon-color="#ef4444"
+  />
+  <naive-icon
+    v-if="device.status === 'unregistered'"
+    :name="ICON_UNKNOWN"
+    :size="size"
+    icon-color="#a3a3a3"
   />
 </template>
 
 <script setup lang="ts">
-const props = defineProps<{ device: Device, size?: number }>()
-
-const icon = computed(() => {
-  switch (props.device.status) {
-    case 'connected':
-      return { name: ICON_CHECK, color: '#22c55e' }
-    case 'disconnected':
-      return { name: ICON_FAIL, color: '#22c55e' }
-    default:
-      return { name: ICON_UNKNOWN, color: '#a3a3a3' }
-  }
-})
+defineProps<{ device: Device, size?: number }>()
 </script>

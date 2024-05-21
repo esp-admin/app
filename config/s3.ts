5add3872c6378662d4a57d5b4df9f91be569348f
becem-gharbi <becem.gharbi@live.com>
2024-05-21 17:01:42 +0100

    chore: change max size for upload to 3Mb

--- config/s3.ts
+++ config/s3.ts
@@ -3,6 +3,6 @@ import type { ModuleOptions } from '@bg-dev/nuxt-s3'
 export const s3: Partial<ModuleOptions> = {
   server: false,
   driver: 'fs',
-  maxSizeMb: 5,
+  maxSizeMb: 3,
   accept: '^(image/(png|jpeg|png|gif|webp))|(application/octet-stream)',
 }

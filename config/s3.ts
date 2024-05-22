8d4b612b284f5446b5e2f7ad167b0476313046eb
becem-gharbi <becem.gharbi@live.com>
2024-05-22 11:33:09 +0100

    revert: set max upload size to 5MB

--- config/s3.ts
+++ config/s3.ts
@@ -3,6 +3,6 @@ import type { ModuleOptions } from '@bg-dev/nuxt-s3'
 export const s3: Partial<ModuleOptions> = {
   server: false,
   driver: 'fs',
-  maxSizeMb: 3,
+  maxSizeMb: 5,
   accept: '^(image/(png|jpeg|png|gif|webp))|(application/octet-stream)',
 }
